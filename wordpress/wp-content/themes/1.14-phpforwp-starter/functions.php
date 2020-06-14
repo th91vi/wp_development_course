fa58148cec54142de72db1b75d09d74a8f9407c1
Thiago Vieira <th91v@outlook.com>
2020-06-14 17:46:25 -0300

    prática com Hooks e Filters; modificação do CSS para o tema

--- wordpress/wp-content/themes/1.14-phpforwp-starter/functions.php
+++ wordpress/wp-content/themes/1.14-phpforwp-starter/functions.php
@@ -5,13 +5,15 @@
  */
 function phpforwp_theme_styles() {
 
-  // Enqueue google fonts https://fonts.googleapis.com/css?family=Open+Sans|Varela+Round
-  // Enque main style sheet (make dependent on google fonts)
+    // Enqueue google fonts https://fonts.googleapis.com/css?family=Open+Sans|Varela+Round
+    wp_enqueue_style('google-font-open-sans','https://fonts.googleapis.com/css?family=Open+Sans|Varela+Round',array(),'false', 'screen');
+    // Enque main style sheet (make dependent on google fonts)
+    wp_enqueue_style('main_style', get_stylesheet_uri(),['google-font-open-sans'],get_the_time(),'screen');
 
 }
 // Add phpforwp_theme_styles function to wp_enqueue_scripts action hook
 // with a priority of 10
-add_action( '', '', 0 );
+add_action( 'wp_enqueue_scripts', 'phpforwp_theme_styles', 10 );
 
 
 /**
@@ -24,15 +26,22 @@ function phpforwp_read_more_link( $excerpt ) {
 
   // Create a variable called $extended_excerpt and
   // assign it the value of $excerpt
+  $extended_excerpt = $excerpt;
 
   // Append a read more link using get_permalink() as the url
+//   echo '<a href="' . get_permalink() . '">Read more &raquo;</a>';
+
+  $extended_excerpt .= ' <a href="' . get_permalink() . '">';
+  $extended_excerpt .= 'Read more &raquo;';
+  $extended_excerpt .= '</a>';
 
   // Return $extended_excerpt
+  return $extended_excerpt;
 
 }
 // Add phpforwp_read_more_link function to the get_the_excerpt
 // with a priority of 10
-add_filter( '', '', 0 );
+add_filter( 'get_the_excerpt', 'phpforwp_read_more_link', 10 );
 
 
 ?>
